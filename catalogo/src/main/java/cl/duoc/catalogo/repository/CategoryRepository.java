61c95d5aa2bb6f21d4d8fddd8530b6cf3a1b61f5
isidora <is.gomezj@duocuc.cl>
2026-05-22 22:47:30 -0400

    Creacion del repositorio con JPA ademas de la creacion de metodos personalizados

---
+++ catalogo/src/main/java/cl/duoc/catalogo/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package cl.duoc.catalogo.repository;
+
+import cl.duoc.catalogo.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    boolean existsByNameIgnoreCase(String name);
+
+    boolean existsByNameIgnoreCaseAndIdNot(String name, Long id);
+
+}
+
